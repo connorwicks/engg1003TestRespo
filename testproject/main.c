61b0403d4854d5f6e9fc4187f2648bf8861ff708
Connor Wicks <c3349617@uon.edu.au>
2020-02-26 18:55:46 +1100

    Initial commit

---
+++ testproject/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  float x;
+  float y;
+  printf("Enter a number: ");
+  scanf("%f", &x); // Note change of %d to %f
+  //Complete the y = line below
+  y = x
+  printf("y: %f\n", y);
+  return 0;
+
+}
+
